164fc4617bfa3b3b1ff544c4db9948caea43783c
dedmazayukr <dedmazyukr@gmail.com>
2017-12-31 01:41:31 +0200

    added color to webgl

--- main.js
+++ main.js
@@ -153,17 +153,28 @@
   const VSHADER_SOUCE = `
     attribute float a_Kx;
     attribute float a_Data;
+    varying float y;
 
     void main() {
+      y = a_Data;
       gl_Position = vec4(a_Kx, a_Data, 0, 1.0);
     }
   `;
 
   const FSHADER_SOURCE = `
-    precision mediump float;
+    precision lowp float;
+    varying float y;
 
+    vec3 hsv2rgb(vec3 c) {
+      vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+      vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+      return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
+    
     void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      vec3 hsv = vec3((1.0 - y)/1.7, 1.0, 1.0);
+      hsv = min(hsv, vec3(1.0, 1.0, 1.0));
+      gl_FragColor = vec4(hsv2rgb(hsv), 1.0);
     }
   `;
 
@@ -209,8 +220,8 @@
   const draw = () => {
     const vertices = new Float32Array(generateGraph(LL));
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
-    gl.drawArrays(gl.POINTS, 0, LL);
-    // gl.drawArrays(gl.LINE_STRIP, 0, LL);
+    // gl.drawArrays(gl.POINTS, 0, LL);
+    gl.drawArrays(gl.LINE_STRIP, 0, LL);
     requestAnimationFrame(draw);
   };
 
